d9bb3150714fa7b3ab84da77f826ef616ff257a3
JP-Gazmuri <jpgazmuri@miuandes.cl>
2024-08-08 16:47:45 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,22 @@ Rails.application.routes.draw do
   resources :tables
   resources :authors do
     get "edit"
+    get "author_summary", on: :collection
   end
+
   resources :sales do
     get "edit"
   end
+
   resources :reviews do
     get "edit"
   end
+
   resources :books do
     get "edit"
+    get "top_rated", on: :collection
+    get "top_selling", on: :collection
+    get "search", on: :collection
   end
+
 end
